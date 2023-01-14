583dc03293ec7d2e746e4a3575e8c1740dd390b0
Mohammed Ihsaanul haque <mohammedihsaanulhaque@Mohammeds-MacBook-Air.local>
2023-01-14 08:05:46 +0530

    pages and routing is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import Category from "./pages/Category";
+import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Listing from "./pages/Listing";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>Hello</header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/create-listing' element={<CreateListing />} />
+        <Route path='/edit-listing' element={<EditListing />} />
+        <Route path='/category/:categoryName' element={<Category />} />
+        <Route
+          path='/category/:categoryName/:listingId'
+          element={<Listing />}
+        />
+      </Routes>
+    </>
   );
 }
 
